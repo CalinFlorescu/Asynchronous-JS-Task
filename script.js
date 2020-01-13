452a9f97381004da7b1361960941928af4409323
Calin.Florescu@travelport.com <calin.florescu@travelport.com>
2020-01-13 10:13:41 +0200

    review

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const ul = document.getElementById("users");
+const ul = document.getElementById("users"); // puncte in plus pentru ca ai tinut chestiile astea ca si constante :D
 const url = "https://api.github.com/users";
 
+// Apreciez ca ai folosit arrow functions, dar nu e neaparat mereu bine. Sa cauti principala diferenta dintre arrow functions si functii normale si sa mi scrii :D
 const createNode = element => document.createElement(element);
 const append = (parent, el) => parent.appendChild(el);
 
-let userFetch = fetch(url)
+let userFetch = fetch(url) // poate fi const pentru ca nu modifici functia niciodata
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    console.log(data); // evita sa lasi console.log() in codul pe care mi-l trimiti, asta e o modalitate sa faci tu debug
     for (key in data) {
       let li = createNode("li");
       let img = createNode("img");
@@ -27,3 +28,6 @@ let userFetch = fetch(url)
   .catch(error => {
     console.log(error);
   });
+
+
+// Felicitari, ai gandit bine implementarea si codul e scris destul de clean, GJ
