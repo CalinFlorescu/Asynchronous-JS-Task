1ca3379faba7ec61a199a7aa62d7ba15d342646b
Calin.Florescu@travelport.com <calin.florescu@travelport.com>
2020-01-13 10:02:15 +0200

    review

--- Asynchronous.js
+++ Asynchronous.js
@@ -1,7 +1,9 @@
 let usersRead  = 0;
+// Per total ai facut o treaba foarte buna, felicitari. Sa te axezi pe partea de scriere a codului, vezi good practices si chestii de genul
+
 const getUsers  = async (url) =>{
   try{
-      let users = [];
+      let users = []; // poate fi const
       const response = await fetch(url);
       if(response.status !== 200)
           throw new Error(response.status);
@@ -9,15 +11,17 @@ const getUsers  = async (url) =>{
       json.forEach(elem=> {users.push(elem)});
       return users;
   }  catch (e) {
+      // Poti adauga un mesaj mai sugestiv de genul console.log(`getUsers error: ${e}`);
       console.log(e)
   }
 };
 let container = document.getElementById("toAdd");
-const myFunction = async () => {
-    console.log(usersRead);
+const myFunction = async () => { // e mai ok sa dai un nume descriptiv functiei
+    console.log(usersRead); // cand mai trimiti cod sa stergi te rog console log-urile
     let url = "https://api.github.com/users?since=" + usersRead.toString();
     console.log(url);
 
+    // try catch in jurul call-ului chiar daca e functia de mai sus, o sa te ajute la debug ca iti dai seama mai repede de unde vine eroarea
     const users = await getUsers(url) ;
 
     for(let user of users){
@@ -41,7 +45,7 @@ const myFunction = async () => {
     }
 
 };
-
+// e ok si daca lasi fara then-ul ala
 myFunction().then();
 
 document.getElementById("next").addEventListener("click", ()=>{
