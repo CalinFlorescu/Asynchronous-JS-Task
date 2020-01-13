b5088381b3d72eae5ca0b7b5602622fe4ec46797
Rainald Balog <rainald.balog@arobs.com>
2020-01-13 03:53:23 -0800

    am rezolvat sarcinile

--- getusers.js
+++ getusers.js
@@ -1,59 +1,56 @@
-const containerDiv = document.getElementsByClassName("container")[0];
+const containerDiv = document.getElementById("container");
 
-// Apreciez ca ai folosit arrow function, dar nu trebuie folosite mereu, exista o diferenta majora intre functiile normale si astea, te rog sa o cauti si sa mi dai mesaj cu ea :D
-createDynamicList = list => {
-    let listLength = list.length;
+function createDynamicList(list) {
+  if (list.length > 0) {
+    let myList = document.createElement("div");
+    myList.className = "oneRow";
+    let counter = 0;
 
-    if (listLength > 0) {
-        let myList = document.createElement("div");
-        myList.className = "oneRow";
+    list.forEach((element, index) => {
+      let listItem = document.createElement("div");
+      listItem.className = "userItem";
 
-        //Te poti folosi de functii din js gen forEach, e mai elegant putin
-        for (let i = 0; i < listLength;) {
-            let listItem = document.createElement("div");
-            listItem.className = "userItem";
+      let image = document.createElement("img");
+      image.src = element.avatar_url;
 
-            let image = document.createElement("img");
-            image.src = list[i].avatar_url;
+      let paragraph = document.createElement("p");
+      paragraph.innerText = "Username: " + element.login;
 
-            let paragraph = document.createElement("p");
-            paragraph.innerText = "Username: " + list[i].login;
-
-            listItem.appendChild(image);
-            listItem.appendChild(paragraph);
-            myList.appendChild(listItem);
-
-            i++;
-
-            if (i % 5 === 0) {
-                containerDiv.appendChild(myList);
-                myList = document.createElement("div");
-                myList.className = "oneRow";
-            }
-        }
-
+      listItem.appendChild(image);
+      listItem.appendChild(paragraph);
+      myList.appendChild(listItem);
+      counter = index + 1;
+      if (counter % 5 === 0 && counter !== 0) {
         containerDiv.appendChild(myList);
-    } else {
-        // Create a text node with the message
-        let message = document.createTextNode("There are no users!");
+        myList = document.createElement("div");
+        myList.className = "oneRow";
+      }
+    });
+    containerDiv.appendChild(myList);
+  } else {
+    // Create a text node with the message
+    let message = document.createTextNode("There are no users!");
 
-        // Append the message to the container
-        containerDiv.appendChild(message);
-    }
-};
+    // Append the message to the container
+    containerDiv.appendChild(message);
+  }
+}
 
-getUsers = async() => {
-    // Foloseste try catch in caz ca iti da eroare
-    const response = await fetch("http://api.github.com/users");
-    const myJson = await response.json();
-    return myJson;
-};
+async function getUsers() {
+  const response = await fetch("http://api.github.com/users")
+    .then(function(users) {
+      return users;
+    })
+    .catch(function(e) {
+      console.log("error in fetch: " + e.message);
+    });
+  const myJson = await response.json();
+  return myJson;
+}
 
-myFunction = async() => { // Da un nume sugestiv functiei
-    const users = await getUsers();
-    createDynamicList(users);
-};
+async function initFunction() {
+  const users = await getUsers();
+  createDynamicList(users);
+}
 
-myFunction();
-
-// Felicitari, per total o tema facuta bine
+initFunction();
